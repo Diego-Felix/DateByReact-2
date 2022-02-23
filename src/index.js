eb817847295bf569c5194f931f15cc892df0c5f7
Diego Felix de Souza <83612963+Diego-Felix@users.noreply.github.com>
2022-02-23 15:54:01 -0300

    ??

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const name = "Diego";
-const year = "2022";
 const data = "23/02/2022";
-
+const img = "bacon.png";
 ReactDOM.render(
   <div>
-    <p>Created by {name}</p>
-    <p>Copyright {year}</p>
+    <img src={img}></img>
+    <h1 class="headding">My Favorit List foods</h1>
+    <ol>
+      <li>Bacon</li>
+      <li>Pasta</li>
+      <li>Rice</li>
+    </ol>
     <p>Today's date is: {data}</p>
   </div>,
   document.getElementById("root")
